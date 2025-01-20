4d6b47041316c35c824252fe09e76947dd72f244
huynguyenj <nguyenhuyjobs@gmail.com>
2025-01-20 13:32:04 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home/Home'
 import UseEffectUseRef from './pages/reactHook/useEffect_useRef/useEffect_useRef'
 import UseReducer from './pages/reactHook/useReducer/UseReducer'
 import UseMemoAndUseCallback from './pages/reactHook/useMemo_useCallback/UseMemo_UseCallback'
+import UseState_Prop from './pages/reactHook/useState_prop/UseState_Prop'
 
 function App() {
 
@@ -15,9 +16,10 @@ function App() {
       <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/useEffect_useRef' element={<UseEffectUseRef />}></Route>
-          <Route path='useReducer' element={<UseReducer/>}></Route>
+          <Route path='/useReducer' element={<UseReducer/>}></Route>
           <Route path='/UnionType_Enum' element={<UnionTypes_Enum/>}></Route>
           <Route path='/useMemo_useCallback' element={<UseMemoAndUseCallback/>}></Route>
+          <Route path='/useState_prop' element={<UseState_Prop/>}></Route>
         </Routes>     
 
     </>
