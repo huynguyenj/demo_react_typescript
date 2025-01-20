41c816dd11d673de63bcfc41ebe79997cfdf5399
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-01-20 11:19:35 +0700

    add Routes back to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import UseMemoAndUseCallback from './pages/reactHook/useMemo_useCallback/UseMemo_UseCallback';
 function App() {
 
   return (
     <>
-      {/* <Routes>
+      <Routes>
           <Route>
 
           </Route>
-        </Routes>      */}
-        <UseMemoAndUseCallback/>
+        </Routes>     
     </>
   )
 }
